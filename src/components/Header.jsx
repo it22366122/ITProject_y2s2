280763bfaf5e488dcf65a89d8bd4f5b45ed180a7
it22366122 <it22366122@my.sliit.lk>
2024-02-14 10:49:47 +0530

    modified:   src/components/Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,25 +4,27 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header className=" bg-gradient-to-r from-cyan-500 to-blue-500 ...	">
+    <header className=" bg-gradient-to-r from-cyan-500 to-blue-500 ... shadow-xl	">
       <div className="flex justify-between items-center max-w-6xl mx -auto p-5">
         <Link to="/">
-          <h1 className="font-bold text-sm:text-xl flex flex-wrap">
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
             <span className="text-slate-600 font-mono">Seni</span>
             <span className="font-mono font-style:bold">Care </span>
             <MdElderlyWoman className=" " />
           </h1>
         </Link>
-        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
-          <input
-            type="text"
-            placeholder="Search..."
-            className="bg-transparent focus:outline-none w-24 sm:w-64"
-          />
-          <button>
-            <FaSearch className="text-slate-600" />
-          </button>
-        </form>
+        
+          <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+            <input
+              type="text"
+              placeholder="Search..."
+              className="bg-transparent focus:outline-none w-24 sm:w-64"
+            />
+            <button>
+              <FaSearch className="text-slate-600" />
+            </button>
+          </form>
+        
         <div>
           <ul className="flex gap-6 font-mono text-slate-50 ">
             <Link to="/">
@@ -42,7 +44,7 @@ export default function Header() {
               </li>
             </Link>
             <Link to="/Signin">
-            <li className=" sm:inline hover:text-slate-500">SignIn </li>
+              <li className=" sm:inline hover:text-slate-500">SignIn </li>
             </Link>
           </ul>
         </div>
