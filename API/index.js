3ec416cf5fbfb524a0b1c22f0bccc24ce54d6b1d
it22366122 <it22366122@my.sliit.lk>
2024-02-14 13:47:56 +0530

    Database connection

--- API/index.js
+++ API/index.js
@@ -1,8 +1,22 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(
+    process.env.DbURI
+  )
+  .then(() => {
+    console.log("connected to mongodb");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
-
-app.listen(3000 , () =>{
-    console.log('Server running on port 3000')
-})
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
